1def118973bbbf19ecfdad8e6d47880ef5ccb9e8
CaedenZ <fengye16@ichat.sp.edu.sg>
2020-10-26 18:01:30 +0800

    auth

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,123 @@
+const User = require("../models/user.model.js");
+const { verify } = require("jsonwebtoken");
+const { hash, compare } = require("bcryptjs");
+const {
+  createAccessToken,
+  createRefreshToken,
+  sendAccessToken,
+  sendRefreshToken,
+} = require("../token.js");
+const { isAuth } = require("../isAuth.js");
+
+// Create and Save a new User
+exports.register = async (req, res) => {
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!",
+    });
+  }
+
+  const hashpassword = await hash(req.body.password, 10);
+  // Create a User
+  const user = new User({
+    email: req.body.email,
+    name: req.body.name,
+    password: hashpassword,
+    score: 0,
+  });
+
+  // Save User in the database
+  User.create(user, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the User.",
+      });
+    else res.send(data);
+  });
+};
+
+// Retrieve all Users from the database.
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  User.findByEmail(email, async (err, data) => {
+    try {
+      if (err) {
+        if (err.kind === "not_found") throw new Error("User not found");
+        else throw new Error("Error when retrieving");
+      } else {
+        console.log(await hash(password, 10));
+        const valid = await compare(password, data.password).catch((e) => {
+          throw e;
+        });
+        console.log(valid);
+        if (!valid) throw new Error("Password not correct");
+        const accesstoken = createAccessToken(data.id);
+        const refreshtoken = createRefreshToken(data.id);
+
+        // put refresh token in db
+        data.refreshtoken = refreshtoken;
+
+        sendRefreshToken(res, refreshtoken);
+        sendAccessToken(res, req, accesstoken);
+      }
+    } catch (err) {
+      res.send({
+        error: `${err.message}`,
+      });
+    }
+  });
+};
+
+// Find a single User with a userId
+exports.logout = (req, res) => {
+  res.clearCookie("refreshtoken", { path: "/refresh_token" });
+  return res.send({
+    message: "Logged out",
+  });
+};
+
+// Update a User identified by the userId in the request
+exports.protected = (req, res) => {
+  // Validate Request
+  try {
+    const userId = isAuth(req);
+    if (userId !== null) {
+      res.send({
+        data: "this is protected data",
+      });
+    }
+  } catch (err) {
+    res.send({
+      error: `${err.message}`,
+    });
+  }
+};
+
+// Delete a User with the specified userId in the request
+exports.refreshToken = (req, res) => {
+  const token = req.cookies.refreshtoken;
+  //if we dun have token
+  if (!token) return res.send({ accesstoken: "" });
+  let payload = null;
+  try {
+    payload = verify(token, process.env.REFRESH_TOKEN_SECRET);
+  } catch (err) {
+    return res.send({ accesstoken: "" });
+  }
+
+  const user = fakeDB.find((user) => user.id === payload.userId);
+  if (!user) return res.send({ accesstoken: "" });
+
+  if (user.refreshtoken !== token) {
+    return res.send({ accesstoken: "" });
+  }
+
+  const accesstoken = createAccessToken(user.id);
+  const refreshtoken = createRefreshToken(user.id);
+
+  user.refreshtoken = refreshtoken;
+
+  sendRefreshToken(res, refreshtoken);
+  return res.send({ accesstoken });
+};
--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ require("dotenv/config");
 const express = require("express");
 const cookieparser = require("cookie-parser");
 const cors = require("cors");
-const { verify } = require("jsonwebtoken");
-const { hash, compare } = require("bcryptjs");
-const { fakeDB } = require("./DB.js");
+const { fakeDB } = require("./models/fakeDB.js");
 const { isAuth } = require("./isAuth.js");
 const {
   createAccessToken,
@@ -30,100 +28,103 @@ server.get("/", (req, res) => {
   res.send({ message: "hi" });
 });
 
-//Register
-server.post("/register", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    //check if user exist
-    const user = fakeDB.find((user) => user.email === email);
-    if (user) throw new Error("User Already Exist");
+// //Register
+// server.post("/register", async (req, res) => {
+//   const { email, password } = req.body;
+//   try {
+//     //check if user exist
+//     const user = fakeDB.find((user) => user.email === email);
+//     if (user) throw new Error("User Already Exist");
 
-    const hashpassword = await hash(password, 10);
+//     const hashpassword = await hash(password, 10);
 
-    fakeDB.push({
-      id: fakeDB.lenth,
-      email,
-      password: hashpassword,
-    });
-    res.send({ message: "User Created" });
-    console.log(fakeDB);
-  } catch (err) {
-    res.send({ error: `${err.message}` });
-  }
-});
+//     fakeDB.push({
+//       id: fakeDB.lenth,
+//       email,
+//       password: hashpassword,
+//     });
+//     res.send({ message: "User Created" });
+//     console.log(fakeDB);
+//   } catch (err) {
+//     res.send({ error: `${err.message}` });
+//   }
+// });
 
-server.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = fakeDB.find((user) => user.email === email);
-    if (!user) throw new Error("User does not exist");
-    const valid = await compare(password, user.password);
-    if (!valid) throw new Error("Password not correct");
-    const accesstoken = createAccessToken(user.id);
-    const refreshtoken = createRefreshToken(user.id);
+// server.post("/login", async (req, res) => {
+//   const { email, password } = req.body;
+//   try {
+//     const user = fakeDB.find((user) => user.email === email);
+//     if (!user) throw new Error("User does not exist");
+//     const valid = await compare(password, user.password);
+//     if (!valid) throw new Error("Password not correct");
+//     const accesstoken = createAccessToken(user.id);
+//     const refreshtoken = createRefreshToken(user.id);
 
-    // put refresh token in db
-    user.refreshtoken = refreshtoken;
-    console.log(fakeDB);
-    //
-    sendRefreshToken(res, refreshtoken);
-    sendAccessToken(res, req, accesstoken);
-  } catch (err) {
-    res.send({
-      error: `${err.message}`,
-    });
-  }
-});
+//     // put refresh token in db
+//     user.refreshtoken = refreshtoken;
+//     console.log(fakeDB);
+//     //
+//     sendRefreshToken(res, refreshtoken);
+//     sendAccessToken(res, req, accesstoken);
+//   } catch (err) {
+//     res.send({
+//       error: `${err.message}`,
+//     });
+//   }
+// });
 
-server.post("/logout", async (_req, res) => {
-  res.clearCookie("refreshtoken", { path: "/refresh_token" });
-  return res.send({
-    message: "Logged out",
-  });
-});
+// server.post("/logout", async (_req, res) => {
+//   res.clearCookie("refreshtoken", { path: "/refresh_token" });
+//   return res.send({
+//     message: "Logged out",
+//   });
+// });
 
-server.post("/protected", async (req, res) => {
-  try {
-    const userId = isAuth(req);
-    if (userId !== null) {
-      res.send({
-        data: "this is protected data",
-      });
-    }
-  } catch (err) {
-    res.send({
-      error: `${err.message}`,
-    });
-  }
-});
+// server.post("/protected", async (req, res) => {
+//   try {
+//     const userId = isAuth(req);
+//     if (userId !== null) {
+//       res.send({
+//         data: "this is protected data",
+//       });
+//     }
+//   } catch (err) {
+//     res.send({
+//       error: `${err.message}`,
+//     });
+//   }
+// });
 
-//get a new access token
-server.post("/refresh_token", async (req, res) => {
-  const token = req.cookies.refreshtoken;
-  //if we dun have token
-  if (!token) return res.send({ accesstoken: "" });
-  let payload = null;
-  try {
-    payload = verify(token, process.env.REFRESH_TOKEN_SECRET);
-  } catch (err) {
-    return res.send({ accesstoken: "" });
-  }
+// //get a new access token
+// server.post("/refresh_token", async (req, res) => {
+//   const token = req.cookies.refreshtoken;
+//   //if we dun have token
+//   if (!token) return res.send({ accesstoken: "" });
+//   let payload = null;
+//   try {
+//     payload = verify(token, process.env.REFRESH_TOKEN_SECRET);
+//   } catch (err) {
+//     return res.send({ accesstoken: "" });
+//   }
 
-  const user = fakeDB.find((user) => user.id === payload.userId);
-  if (!user) return res.send({ accesstoken: "" });
+//   const user = fakeDB.find((user) => user.id === payload.userId);
+//   if (!user) return res.send({ accesstoken: "" });
 
-  if (user.refreshtoken !== token) {
-    return res.send({ accesstoken: "" });
-  }
+//   if (user.refreshtoken !== token) {
+//     return res.send({ accesstoken: "" });
+//   }
 
-  const accesstoken = createAccessToken(user.id);
-  const refreshtoken = createRefreshToken(user.id);
+//   const accesstoken = createAccessToken(user.id);
+//   const refreshtoken = createRefreshToken(user.id);
 
-  user.refreshtoken = refreshtoken;
+//   user.refreshtoken = refreshtoken;
 
-  sendRefreshToken(res, refreshtoken);
-  return res.send({ accesstoken });
-});
+//   sendRefreshToken(res, refreshtoken);
+//   return res.send({ accesstoken });
+// });
+
+require("./routes/user.routes.js")(server);
+require("./routes/auth.routes.js")(server);
 
 server.listen(process.env.PORT, () =>
   console.log(`Server listening on port ${process.env.PORT}`)
