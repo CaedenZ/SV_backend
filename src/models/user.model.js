62fceec877c5645b7139f73d21a53d2bf8f64f1c
CaedenZ <fengye16@ichat.sp.edu.sg>
2020-11-02 11:53:18 +0800

    selectuser

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -23,22 +23,25 @@ User.create = (newUser, result) => {
 };
 
 User.findById = (userId, result) => {
-  sql.query(`SELECT * FROM users WHERE id = ${userId}`, (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-      return;
-    }
+  sql.query(
+    `SELECT id, email, name, score, type FROM users WHERE id = ${userId}`,
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
 
-    if (res.length) {
-      console.log("found user: ", res[0]);
-      result(null, res[0]);
-      return;
-    }
+      if (res.length) {
+        console.log("found user: ", res[0]);
+        result(null, res[0]);
+        return;
+      }
 
-    // not found User with the id
-    result({ kind: "not_found" }, null);
-  });
+      // not found User with the id
+      result({ kind: "not_found" }, null);
+    }
+  );
 };
 
 User.findByEmail = (email, result) => {
