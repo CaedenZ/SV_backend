010bca68fd8dec364890587bf6b7a1608c527757
CaedenZ <fengye16@ichat.sp.edu.sg>
2020-12-15 06:37:46 +0800

    fix

--- src/routes/team.routes.js
+++ src/routes/team.routes.js
@@ -8,9 +8,6 @@ module.exports = (app) => {
   app.get("/teams", teams.findAll);
 
   // Retrieve a single Customer with teamId
-  app.get("/teams/:teamId", teams.findByID);
-
-  // Retrieve a single Customer with teamId
   app.get("/teams/:gID", teams.findBGid);
 
   // Update a Customer with teamId
